d5d5f499ec6f079ef0d99b37eec773d5f6bebb3c
Minh Tran <mdtran@cpp.edu>
2022-10-22 14:12:10 -0700

    Tested loShu and non-loShu with isLoShuMagicSquare function

--- loShuDemo.c
+++ loShuDemo.c
@@ -17,20 +17,38 @@ int main()
 
 
     // Test a Lo Shu Square here
+    printf("Lo Shu Square (should return True): ");
     int ls[][3] = {
         {4, 9, 2},
         {3, 5, 7},
         {8, 1, 6},
     };
-
-    bool isLoShu = isLoShuMagicSquare(ls);
+    if(isLoShuMagicSquare(ls))
+    {
+        printf("True\n");
+    }
+    else
+    {
+        printf("False\n");
+    }
 
     // Test a non-Lo Shu Square here
+    printf("Non-Lo Shu Square (should return False): ");
     int nls[][3] = {
-        {4, 9, 2},
-        {3, 5, 7},
-        {8, 1, 6},
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
     };
+    if(isLoShuMagicSquare(nls))
+    {
+        printf("True\n");
+    }
+    else
+    {
+        printf("False\n");
+    }
+    printf("\n");
+
 
     return EXIT_SUCCESS;
 }
