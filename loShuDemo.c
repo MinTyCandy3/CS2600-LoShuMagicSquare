58c0f4a2c3b8dc1dfad37953f6def3299a2ba1f3
Minh Tran <mdtran@cpp.edu>
2022-10-22 12:50:08 -0700

    added main file, provided outline

---
+++ loShuDemo.c
@@ -0,0 +1,19 @@
+
+
+// code main here
+
+// Test a Lo Shu Square here
+
+// Test a non-Lo Shu Square here
+
+// Test populating 2D arrays and check and loop until Lo Shu
+
+
+// add header file to define/struct Lo Shu Magic Square
+
+
+// functions here (maybe in a separate file)
+
+// ** create a function that takes in a 2D array, and determine if it's a Lo Shu
+
+// function that populates an empty 2D array with random numbers
